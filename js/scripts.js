c065b155dfe48a6625adac77ab481caac931ebe1
alvsgabriellk <cauagabriell81@gmail.com>
2026-02-18 08:38:27 -0300

    Iniciando o js com variáveis dos objetos/dados do form

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+const form = document.querySelector("#form")
+const nameinput = document.querySelector("#name")
+const emailinput = document.querySelector("#email")
+const password = document.querySelector("#password")
+const jobselect = document.querySelector("#job")
+const messagetextarea = document.querySelector("#message")
+
